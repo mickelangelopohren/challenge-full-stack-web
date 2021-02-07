4275a971d46c7183be8694c66ed8dc35fb8183b0
mickelangelopohren <mickelangelo.pohren@gmail.com>
2021-02-07 17:06:13 -0300

    backend: Add sqlite to sequelize database config

--- resolution/backend/src/config/database.js
+++ resolution/backend/src/config/database.js
@@ -4,6 +4,7 @@ module.exports = {
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   dialect: process.env.NODE_ENV === 'test' ? 'sqlite' : 'mysql',
+  storage: './tests/database.sqlite',
   operatorsAliases: 0,
   logging: false,
   define: {
