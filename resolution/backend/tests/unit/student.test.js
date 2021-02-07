4e94427e6fd89437417d039f581ab6614c16038b
mickelangelopohren <mickelangelo.pohren@gmail.com>
2021-02-07 18:18:54 -0300

    backend: Add creation error teset - phase 1

--- resolution/backend/tests/unit/student.test.js
+++ resolution/backend/tests/unit/student.test.js
@@ -30,5 +30,21 @@ describe('Students service', () => {
 
     expect(createdStudent?.id).toBeDefined();
   });
+
+  it('Should return an error on duplicated academicRegister entry', async () => {
+    const data1 = dataMock();
+    const data2 = dataMock({
+      name: 'Marina Miranda',
+      email: 'marina.miranda@teste.com',
+      document: '36508685040'
+    });
+
+    const createdStudent1 = await StudentService.create({ data: data1 })
+    try {
+    const createdStudent2 = await StudentService.create({ data: data2 })
+    } catch(error) {
+      expect(error).toBeInstanceOf(Error);
+    }
+  });
 });
 
