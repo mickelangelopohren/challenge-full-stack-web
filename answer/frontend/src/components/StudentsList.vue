11c4fd6458a18f4f953c18b384cdb43f5829088a
mickelangelopohren <mickelangelo.pohren@gmail.com>
2021-02-08 19:52:04 -0300

    frontend: Create list component

--- answer/frontend/src/components/StudentsList.vue
+++ answer/frontend/src/components/StudentsList.vue
@@ -1,12 +1,103 @@
 <template>
-  <v-container>
-  </v-container>
+  <v-row align="center" class="list px-3 mx-auto">
+    <v-col cols="12" md="8">
+      <v-text-field v-model="title" label="Consulta de Alunos"></v-text-field>
+    </v-col>
+
+    <v-col cols="12" sm="12">
+      <v-card class="mx-auto" tile>
+
+        <v-card-title>
+          <v-text-field
+            v-model="search"
+            append-icon="mdi-magnify"
+            label="Search"
+            single-line
+            hide-details
+            >
+          </v-text-field>
+
+            <v-btn
+              color="primary"
+              dark
+              class="mb-2"
+              @click="createStudent()"
+              >
+              Cadastrar Aluno
+            </v-btn>
+        </v-card-title>
+
+        <v-data-table
+          :headers="headers"
+          :items="students"
+          :search="search"
+          >
+          <template v-slot:[`item.actions`]="{ item }">
+            <v-icon small class="mr-2" @click="editStudent(item.id)">mdi-pencil</v-icon>
+          <v-icon small @click="deleteStudent(item.id)">mdi-delete</v-icon>
+          </template>
+        </v-data-table>
+
+      </v-card>
+    </v-col>
+  </v-row>
 </template>
 
 <script>
-  export default {
+import Backend from "../services/Backend"
 
-    data: () => ({
-    }),
-  }
+export default {
+  name: "studants-list",
+  data() {
+    return {
+      search: '',
+      students: [],
+      headers: [
+        { text: "Registro acadêmico", value: "academicRegister" },
+        { text: "Nome", value: "name" },
+        { text: "CPF", value: "document" },
+        { text: 'Ações', value: 'actions', sortable: false },
+      ],
+    }
+  },
+  methods: {
+    retrieveStudents() {
+      Backend.getAll()
+        .then((response) => {
+          this.students = response.data
+        })
+        .catch((e) => {
+          console.log(e)
+        })
+    },
+    refreshList() {
+      this.retrieveStudents()
+    },
+    editStudent(id) {
+      this.$router.push({ name: "student-edit", params: { id: id } })
+    },
+    deleteStudent(id) {
+      Backend.remove(id)
+        .then(() => {
+          this.refreshList()
+        })
+        .catch((e) => {
+          console.log(e)
+        })
+    },
+    createStudent() {
+      this.$router.push({ name: "student-create" })
+    }
+  },
+  mounted() {
+    this.retrieveStudents()
+  },
+}
 </script>
+
+<style>
+.list {
+  max-width: 70%;
+}
+</style>
+
