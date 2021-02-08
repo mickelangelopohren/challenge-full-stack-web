7492a1d04ae02d5ffdaffe4b9addb548c51dbee0
mickelangelopohren <mickelangelo.pohren@gmail.com>
2021-02-08 19:41:37 -0300

    frontend: Create a backend request service

---
+++ answer/frontend/src/services/Backend.js
@@ -0,0 +1,28 @@
+import axios from "axios"
+
+const baseUrl = process.env.VUE_APP_API_URL
+
+class TutorialDataService {
+
+  getAll() {
+    return axios.get(`${baseUrl}/students`)
+  }
+
+  getOne(id) {
+    return axios.get(`${baseUrl}/students/${id}`)
+  }
+
+  create(data) {
+    return axios.post(`${baseUrl}/students`, data)
+  }
+
+  update(id, data) {
+    return axios.patch(`${baseUrl}/students/${id}`, data)
+  }
+
+  remove(id) {
+    return axios.delete(`${baseUrl}/students/${id}`)
+  }
+}
+
+export default new TutorialDataService()
