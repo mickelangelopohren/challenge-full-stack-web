ba0b83a7bad987eb11fd00d753b5f1316d7ed2d0
mickelangelopohren <mickelangelo.pohren@gmail.com>
2021-02-08 15:06:43 -0300

    frontend: Create initial student view

--- answer/frontend/src/App.vue
+++ answer/frontend/src/App.vue
@@ -4,53 +4,36 @@
       app
       color="primary"
       dark
-    >
+      >
       <div class="d-flex align-center">
         <v-img
-          alt="Vuetify Logo"
+          alt="Grupo A Logo"
           class="shrink mr-2"
           contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          src="./assets/logo.webp"
           transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
           width="100"
-        />
+          />
       </div>
 
       <v-spacer></v-spacer>
 
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
     </v-app-bar>
 
     <v-main>
-      <HelloWorld/>
+      <StudentsList/>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import StudentsList from './components/StudentsList';
 
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
+    StudentsList,
   },
 
   data: () => ({
---
+++ answer/frontend/src/views/StudentsList.vue
@@ -0,0 +1,13 @@
+<template>
+  <StudentsList/>
+</template>
+
+<script>
+import StudentsList from '@/components/StudentsList.vue'
+
+export default {
+  components: {
+    StudentsList
+  }
+}
+</script>
