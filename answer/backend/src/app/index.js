bdbcf10115fcd37514e4e60ca0a1cc9037614013
mickelangelopohren <mickelangelo.pohren@gmail.com>
2021-02-08 14:42:30 -0300

    backend: Fix validation error response

--- answer/backend/src/app/index.js
+++ answer/backend/src/app/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { ValidationError } = require('express-validation');
 
 const app = express();
 
@@ -7,5 +8,13 @@ app.use(cors());
 app.use(express.json());
 
 app.use(require('./routes'));
+
+app.use((err, req, res) => {
+  if (err instanceof ValidationError) {
+    return res.status(err.statusCode).send(err);
+  }
+
+  return res.status(500).send(err);
+});
 
 module.exports = app;
--- answer/backend/src/errors.js
+++ answer/backend/src/errors.js
@@ -1,3 +1,4 @@
+/* eslint max-classes-per-file: ["error", 2] */
 class BusinessLogicError extends Error {
   constructor(message) {
     super(message);
