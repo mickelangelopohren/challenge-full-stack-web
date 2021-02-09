04b3caa51c3d96cfab2f082f8179717dd847b008
mickelangelopohren <mickelangelo.pohren@gmail.com>
2021-02-09 15:01:28 -0300

    frontend: Create delete sutudent confirm dialog

--- answer/frontend/src/components/StudentsList.vue
+++ answer/frontend/src/components/StudentsList.vue
@@ -24,17 +24,30 @@
             </v-btn>
         </v-card-title>
 
-        <v-data-table
-          :headers="headers"
-          :items="students"
-          :search="search"
-          hide-default-footer
-          >
-          <template v-slot:[`item.actions`]="{ item }">
-            <v-icon small class="mr-2" @click="editStudent(item.id)">mdi-pencil</v-icon>
-          <v-icon small @click="deleteStudent(item.id)">mdi-delete</v-icon>
-          </template>
-        </v-data-table>
+        <v-dialog v-model="dialogDelete" max-width="540px">
+          <v-card class="text-right">
+            <v-card-title class="headline">Tem certeza de que deseja excluir este aluno</v-card-title>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn color="blue darken-1" text @click="closeDelete">Cancelar</v-btn>
+              <v-btn color="blue darken-1" text @click="deleteConfirm">Confirmar</v-btn>
+              <v-spacer></v-spacer>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+
+      <v-data-table
+        :headers="headers"
+        :items="students"
+        :search="search"
+        hide-default-footer
+        >
+
+        <template v-slot:[`item.actions`]="{ item }">
+          <v-icon small class="mr-2" @click="editStudent(item.id)">mdi-pencil</v-icon>
+        <v-icon small @click="deleteStudent(item.id)">mdi-delete</v-icon>
+        </template>
+      </v-data-table>
 
       </v-card>
     </v-col>
@@ -56,6 +69,8 @@ export default {
         { text: "CPF", value: "document" },
         { text: 'Ações', value: 'actions', sortable: false },
       ],
+      dialogDelete: false,
+      studentIdToDelete: '',
     }
   },
   methods: {
@@ -75,20 +90,34 @@ export default {
       this.$router.push({ name: "student-edit", params: { id: id } })
     },
     deleteStudent(id) {
-      Backend.remove(id)
+      this.dialogDelete = true
+      this.studentIdToDelete = id
+    },
+    deleteConfirm () {
+      Backend.remove(this.studentIdToDelete)
         .then(() => {
           this.refreshList()
         })
         .catch((e) => {
           console.log(e)
         })
+      this.closeDelete()
+    },
+    closeDelete () {
+      this.dialogDelete = false
+      this.studentIdToDelete = ''
     },
     createStudent() {
       this.$router.push({ name: "student-create" })
-    }
+    },
   },
   mounted() {
     this.retrieveStudents()
+  },
+  watch: {
+    dialogDelete (value) {
+      value || this.closeDelete()
+    },
   },
 }
 </script>
