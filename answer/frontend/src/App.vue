d5edad0ce0899f0f0556519f204afa2d2d28f44d
mickelangelopohren <mickelangelo.pohren@gmail.com>
2021-02-09 10:57:00 -0300

    frontend: Create asinde menu

--- answer/frontend/src/App.vue
+++ answer/frontend/src/App.vue
@@ -15,13 +15,35 @@
           width="100"
           />
       </div>
-
-      <v-spacer></v-spacer>
-
     </v-app-bar>
 
+
     <v-main>
+    <v-card>
+    <v-navigation-drawer
+      absolute
+      permanent
+      left
+    >
+
+      <v-list dense class="mt-10">
+        <v-list-item
+          v-for="item in items"
+          :key="item.title"
+        >
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
        <router-view />
+  </v-card>
     </v-main>
   </v-app>
 </template>
@@ -35,7 +57,9 @@ export default {
   },
 
   data: () => ({
-    //
+        items: [
+          { title: 'Alunos', icon: 'mdi-account-group-outline' },
+        ],
   }),
 };
 </script>
